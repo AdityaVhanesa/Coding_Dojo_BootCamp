145e8fecf2e46e40c7acaeec4bfd1d07a0a19c8a
Aditya Vhanesa <adityaVha@gmail.com>
2022-06-11 20:54:50 -0500

    FLASK + SQL -------------------------------- Assignment --> Users CURD Type       --> Practice

--- Python_Stack/flask_sql/FLASK_User_CR/app.py
+++ Python_Stack/flask_sql/FLASK_User_CR/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect
+
 from users import USER
+
 app = Flask(__name__)
 
 
@@ -7,7 +9,7 @@ app = Flask(__name__)
 def index():  # put application's code here
     users_dict = USER.all()
     print(users_dict)
-    return render_template('index.html', users = users_dict)
+    return render_template('index.html', users=users_dict)
 
 
 @app.route('/add_user')
@@ -15,7 +17,7 @@ def add_user():
     return render_template('create_friend.html')
 
 
-@app.route('/create_user', methods = ['POST'])
+@app.route('/create_user', methods=['POST'])
 def create_user():
     user_dict = {
         "first_name": request.form.get('first_name'),
@@ -24,6 +26,35 @@ def create_user():
     }
     USER.add(user_dict)
     return redirect('/')
+
+
+@app.route('/users/<int:user_id>')
+def showUser(user_id):
+    userObject = USER.getUserById({"id": user_id})
+    return render_template("show_user.html", user=userObject)
+
+
+@app.route('/users/<int:user_id>/delete')
+def removeUser(user_id):
+    USER.removeUserById({"id": user_id})
+    return redirect("/")
+
+
+@app.route('/users/<int:user_id>/edit')
+def updateUserForm(user_id):
+    userObject = USER.getUserById({"id": user_id})
+    return render_template("update_user.html", user=userObject)
+
+
+@app.route('/users/<int:user_id>/update', methods=["POST"])
+def updateUser(user_id):
+    USER.updateUserById({"id": user_id,
+                         "first_name": request.form.get("first_name"),
+                         "last_name": request.form.get("last_name"),
+                         "email": request.form.get("email")
+                        })
+
+    return redirect(f"/users/{user_id}")
 
 
 if __name__ == '__main__':
--- Python_Stack/flask_sql/FLASK_User_CR/users.py
+++ Python_Stack/flask_sql/FLASK_User_CR/users.py
@@ -1,15 +1,22 @@
-from datetime import datetime
-
 from connection import SQL
 
 
-class date:
+class createDate:
     def __init__(self, dateTime):
         self.days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
         self.dateTime = dateTime
 
     def __str__(self):
         return f"{self.days[self.dateTime.weekday()]} {self.dateTime.day}, {self.dateTime.year}"
+
+
+class updateDate:
+    def __init__(self, dateTime):
+        self.days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        self.dateTime = dateTime
+
+    def __str__(self):
+        return f"{self.days[self.dateTime.weekday()]} {self.dateTime.day}, {self.dateTime.year} at {self.dateTime.time()}"
 
 
 class USER:
@@ -20,8 +27,8 @@ class USER:
         self.first_name = data['first_name']
         self.last_name = data['last_name']
         self.email = data['email']
-        self.created_at = date(data['created_at'])
-        self.updated_at = date(data['updated_at'])
+        self.created_at = createDate(data['created_at'])
+        self.updated_at = updateDate(data['updated_at'])
 
     @classmethod
     def all(cls):
@@ -41,3 +48,21 @@ class USER:
     def add(cls, data):
         query = 'INSERT INTO users(first_name, last_name, email) VALUE (%(first_name)s, %(last_name)s, %(email)s);'
         return SQL(cls.dataBase).query(query, data)
+
+    @classmethod
+    def getUserById(cls, data):
+        query = 'SELECT * FROM users WHERE id = %(id)s;'
+        result = SQL(cls.dataBase).query(query, data)
+        if result:
+            return cls(result[0])
+
+    @classmethod
+    def removeUserById(cls, data):
+        query = "DELETE FROM users WHERE id = %(id)s;"
+        SQL(cls.dataBase).query(query, data)
+
+    @classmethod
+    def updateUserById(cls, data):
+        query = "UPDATE users SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s WHERE id = %(id)s;"
+        SQL(cls.dataBase).query(query, data)
+
